f3c8a98bfcaab6d9c949ffa248b3ae959c7c7258
cjdsie <cjdsie@gmail.com>
2012-07-17 15:37:21 -0230

    Responsive Slideshow added

--- js/script.js
+++ js/script.js
@@ -105,5 +105,34 @@ $("<select />").appendTo("nav.menu1");
   }
 
 
+
+	// Slideshow 1
+    $("#slider1").responsiveSlides({
+      auto: false,
+      pager: true,
+      nav: true,
+      speed: 500,
+      namespace: "centered-btns"
+    });
+
+    // Slideshow 2
+    $("#slider2").responsiveSlides({
+      auto: false,
+      pager: true,
+      nav: true,
+      speed: 500,
+      namespace: "transparent-btns"
+    });
+
+    // Slideshow 3
+    $("#slider3").responsiveSlides({
+      auto: false,
+      pager: false,
+      nav: true,
+      speed: 500,
+      namespace: "large-btns"
+    });
+
+
  
  });
