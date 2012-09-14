1e858ba20b5acb847e10e950e7f83969397e1b30
cjdsie <cjdsie@gmail.com>
2012-09-14 07:56:22 -0230

    Commit build 2.0.1

--- js/tabs.js
+++ js/tabs.js
@@ -1,12 +1,4 @@
 
-/**
- * Skeleton V1.1
- * Copyright 2011, Dave Gamache
- * www.getskeleton.com
- * Free to use under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 8/17/2011
- */
 
 (function ($) {
   // hash change handler
