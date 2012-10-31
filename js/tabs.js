702724c76c686e60f23dd469492cb417713fd2e1
cjdsie <cjdsie@gmail.com>
2012-10-31 08:50:46 -0230

    Added Gallery class to features

--- js/tabs.js
+++ js/tabs.js
@@ -1,13 +1,3 @@
-
-/**
- * Skeleton V1.1
- * Copyright 2011, Dave Gamache
- * www.getskeleton.com
- * Free to use under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 8/17/2011
- */
-
 (function ($) {
   // hash change handler
   function hashchange () {
